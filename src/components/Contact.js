de788f2c27913f5bc811837936ee93319ec20a1d
Stefan <stefan.paiu.0595@gmail.com>
2023-08-15 11:15:45 +0200

    Nav bar fixed. Text aligned in contact section. Changed header icon colour

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -11,10 +11,10 @@ function Contact (){
                 <p className="mb-8 px-8 lg:mb-16 font-light text-center text-gray-500 dark:text-gray-400 sm:text-xl">Ne puteți contact la telefon sau pe mail pentru informații legate de Safari, dar și pentru orice alte nedumeriri.</p>
                 <div className="grid md:grid-cols-2 sm:grid-cols-1 md:text-xl ">
                     <a href="tel:+40744266714" className="col-span-1 pb-4 md:pb-0">
-                        <p className="text-paragraph font-semibold text-center"><FontAwesomeIcon icon={faPhone} size="xl" className="text-water-blue-dark mr-8" />+40744266714</p>
+                        <p className="text-paragraph font-semibold text-center md:text-left"><FontAwesomeIcon icon={faPhone} size="xl" className="text-water-blue-dark mr-8" />+40744266714</p>
                     </a>
                     <a href="mailto:robert-mihai.amarinei@unige.ch" className="col-span-1 pt-4 md:pt-0">
-                        <p className="text-paragraph font-semibold text-center"><FontAwesomeIcon icon={faEnvelope} size="xl" className="text-water-blue-dark mr-8" />robert-mihai.amarinei@unige.ch</p>
+                        <p className="text-paragraph font-semibold text-center md:text-right"><FontAwesomeIcon icon={faEnvelope} size="xl" className="text-water-blue-dark mr-8" />robert-mihai.amarinei@unige.ch</p>
                     </a>
                 </div>
             </div>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -15,43 +15,44 @@ const sections = [
     },
 ]
 
-const Navbar = () => {
+function Navbar ({fixed}) {
+    const [navbarOpen, setNavbarOpen] = React.useState(false);
     return (
         <nav className="bg-white border-gray-200 bg-white">
             <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
                 {/*! Temporary solution ! -- The extra div.*/}
-                <div className="flex justify-start"></div>
-                <div className="flex justify-center hidden w-full md:flex md:w-auto md:order-1"
+                <div className="flex flex-column justify-between md:order-2 w-full md:w-auto items-center">
+                    <a href="#contact"
+                       className="text-white bg-highlight-blue-dark font-medium rounded-lg text-sm px-4 py-2 text-center mr-3 hover:bg-highlight-blue-light focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-highlight-blue-light dark:hover:bg-highlight-blue-dark dark:focus:ring-blue-800">Contact
+                    </a>
+                    <div className="flex">
+                        <button data-collapse-toggle="navbar-cta" type="button"
+                                className="inline-flex md:items-center sm:items-start p-2 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+                                aria-controls="navbar-cta" aria-expanded="false"
+                                onClick={() => setNavbarOpen(!navbarOpen)}>
+                            <span className="sr-only">Open main menu</span>
+                            <svg className="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20"
+                                 xmlns="http://www.w3.org/2000/svg">
+                                <path fill-rule="evenodd"
+                                      d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
+                                      clip-rule="evenodd"></path>
+                            </svg>
+                        </button>
+                    </div>
+                </div>
+                <div className={"justify-center w-full md:flex md:w-auto md:order-1 " + (navbarOpen ? "" : "hidden")}
                      id="navbar-cta">
-                    <ul className="flex flex-col font-medium p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 dark:border-gray-700">
+                    <ul className="flex flex-col font-medium p-4 md:p-0 my-4 md:mb-0 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 dark:border-gray-700">
                         {sections.map(section => (
                         <li>
                             <a href={section.link}
-                               className="block py-2 pl-3 pr-4 text-water-blue-dark rounded hover:bg-gray-100 hover:text-water-blue-medium-dark md:hover:bg-transparent md:hover:water-blue-medium md:p-0 md:dark:hover:text-water-blue-medium dark:water-blue-dark dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
+                               className="block py-2 pl-3 pr-4 text-water-blue-dark text-center rounded hover:bg-gray-100 hover:text-water-blue-medium-dark md:hover:bg-transparent md:hover:water-blue-medium md:p-0 md:dark:hover:text-water-blue-medium dark:water-blue-dark dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
                                aria-current="page">{section.title}</a>
                         </li>
                         ))}
                     </ul>
                 </div>
-                <div className="flex md:order-3">
-                <a href="#contact"
-                        className="text-white bg-highlight-blue-dark hover:bg-highlight-blue-light focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center mr-3 md:mr-0 dark:bg-highlight-blue-light dark:hover:bg-highlight-blue-dark dark:focus:ring-blue-800">Contact
-                </a>
-                </div>
-                <div className="flex md:order-4">
 
-                    <button data-collapse-toggle="navbar-cta" type="button"
-                            className="inline-flex md:items-center sm:items-start p-2 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
-                            aria-controls="navbar-cta" aria-expanded="false">
-                        <span className="sr-only">Open main menu</span>
-                        <svg className="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20"
-                             xmlns="http://www.w3.org/2000/svg">
-                            <path fill-rule="evenodd"
-                                  d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
-                                  clip-rule="evenodd"></path>
-                        </svg>
-                    </button>
-                </div>
             </div>
         </nav>
     )
